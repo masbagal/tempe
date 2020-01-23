26520e778108724e7f72e3d99d4655372c26b835
masbagal <dimitrij>
2020-01-23 18:53:08 +0700

    refactor to make it lighter, bump version

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Locale } from '../types';
-import { parseStringFormat } from './utils';
+import { parseStringFormat, translateValue } from './utils';
 
 class TempeDate {
   date: Date;
@@ -34,7 +34,8 @@ class TempeDate {
   ) {
     const values = stringFormats.reduce(
       (valueMap: any, currentStringFormat) => {
-        valueMap[currentStringFormat] = this._formatter(
+        valueMap[currentStringFormat] = translateValue(
+          this.date,
           currentStringFormat,
           locale
         );
@@ -43,46 +44,6 @@ class TempeDate {
       {}
     );
     return values;
-  }
-
-  _formatter(stringFormat: string, locale: Locale) {
-    const formatters: any = {
-      YYYY: () => this.date.toLocaleString(locale, { year: 'numeric' }),
-      YYY: () => this.date.toLocaleString(locale, { year: 'numeric' }),
-      YY: () => this.date.toLocaleString(locale, { year: '2-digit' }),
-      MMMM: () => this.date.toLocaleString(locale, { month: 'long' }),
-      MMM: () => this.date.toLocaleString(locale, { month: 'short' }),
-      MM: () => this.date.toLocaleString(locale, { month: '2-digit' }),
-      M: () => this.date.toLocaleString(locale, { month: 'numeric' }),
-      DD: () => this.date.toLocaleString(locale, { day: '2-digit' }),
-      D: () => this.date.toLocaleString(locale, { day: 'numeric' }),
-      dd: () => this.date.toLocaleString(locale, { weekday: 'long' }),
-      d: () => this.date.toLocaleString(locale, { weekday: 'short' }),
-      hh: () =>
-        this.date
-          .toLocaleString(locale, { hour: 'numeric', hour12: false })
-          .padStart(2, '0'),
-      h: () =>
-        this.date.toLocaleString(locale, { hour: 'numeric', hour12: false }),
-      HH: () =>
-        this.date.toLocaleString(locale, { hour: 'numeric', hour12: true }),
-      H: () =>
-        this.date.toLocaleString(locale, { hour: 'numeric', hour12: true }),
-      mm: () =>
-        this.date
-          .toLocaleString(locale, { minute: 'numeric' })
-          .padStart(2, '0'),
-      m: () => this.date.toLocaleString(locale, { minute: 'numeric' }),
-      ss: () =>
-        this.date
-          .toLocaleString(locale, { second: 'numeric' })
-          .padStart(2, '0'),
-      s: () => this.date.toLocaleString(locale, { second: 'numeric' }),
-    };
-
-    const formatter = formatters[stringFormat];
-    const receivedValue = formatter();
-    return receivedValue;
   }
 }
 
--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,38 @@ export function parseStringFormat(stringFormat: string): Array<string> {
   const matchedFormats = stringFormat.match(regexp) || [];
   return matchedFormats;
 }
+
+const PADDED_FORMATS: Array<string> = ['hh', 'DD', 'mm', 'ss', 'hh'];
+const FORMATS: any = {
+  YYYY: { year: 'numeric' },
+  YYY: { year: 'numeric' },
+  YY: { year: '2-digit' },
+  MMMM: { month: 'long' },
+  MMM: { month: 'short' },
+  MM: { month: '2-digit' },
+  M: { month: 'numeric' },
+  DD: { day: '2-digit' },
+  D: { day: 'numeric' },
+  dd: { weekday: 'long' },
+  d: { weekday: 'short' },
+  hh: { hour: 'numeric', hour12: false },
+  h: { hour: 'numeric', hour12: false },
+  HH: { hour: 'numeric', hour12: true },
+  H: { hour: 'numeric', hour12: true },
+  mm: { minute: 'numeric' },
+  m: { minute: 'numeric' },
+  ss: { second: 'numeric' },
+  s: { second: 'numeric' },
+};
+
+export function translateValue(
+  dateObject: Date,
+  stringFormat: string,
+  locale: string
+) {
+  const format = FORMATS[stringFormat];
+  const value = dateObject.toLocaleString(locale, format);
+
+  const shouldPad = PADDED_FORMATS.includes(stringFormat);
+  return shouldPad ? value.padStart(2, '0') : value;
+}
