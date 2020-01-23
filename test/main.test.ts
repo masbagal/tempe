844c979538880af49affe1964a050d2ff30cf694
masbagal <dimitrij>
2020-01-23 18:40:50 +0700

    update test

--- test/main.test.ts
+++ test/main.test.ts
@@ -23,27 +23,11 @@ describe('Main function', () => {
     const format1 = 'dd, DD MMM YYYY';
     const date1 = tempeDate.format(format1);
     expect(date1).toEqual('Sunday, 13 Jan 2019');
-
-    const format2 = 'M MMM MMMM';
-    const date2 = tempeDate.format(format2);
-    expect(date2).toEqual('1 Jan January');
-
-    const format3 = 'hh:mm:ss HH m:s';
-    const date3 = tempeDate.format(format3);
-    expect(date3).toEqual('08:05:03 8 AM 5:3');
   });
 
   it('should initialize and able to chain format the date correctly', () => {
     const format1 = 'dd, DD MMM YYYY';
     const tempeDate1 = tempe(2019, 0, 13, 8, 5, 3).format(format1);
     expect(tempeDate1).toEqual('Sunday, 13 Jan 2019');
-
-    const format2 = 'M MMM MMMM';
-    const tempeDate2 = tempe(2019, 0, 13, 8, 5, 3).format(format2);
-    expect(tempeDate2).toEqual('1 Jan January');
-
-    const format3 = 'hh:mm:ss HH m:s';
-    const tempeDate3 = tempe(2019, 0, 13, 8, 5, 3).format(format3);
-    expect(tempeDate3).toEqual('08:05:03 8 AM 5:3');
   });
 });
